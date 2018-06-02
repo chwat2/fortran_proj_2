844288954e9dd9a2b677b61d66e08e95741e3815
Unknown <albertpawula@gmail.com>
2018-06-02 02:57:44 +0200

    Add makeSimulation subroutine
    TODO: write some tests, write procedure to startSimulations&log results & draw chart

--- mult.f90
+++ mult.f90
@@ -4,6 +4,73 @@ module mult
 
 contains
 
+  subroutine makeSimulation(N)
+    implicit none
+
+      real,allocatable :: m1(:,:), m2(:,:),result(:,:);
+
+      integer, intent(in) :: N;
+      integer :: i,j;
+      real (kind = 8) :: k;
+      integer :: error;
+integer (kind = 4):: iclock;
+real (kind = 8) :: start,finish
+
+      allocate(m1(N,N));
+      allocate(m2(N,N));
+      allocate(result(N,N));
+
+      error = 0;
+
+      k=1.d0;
+      do i = 1, N
+          do j = 1, N
+                  m1(i,j) = k;
+                  k = k + 1.d0;
+          end do
+      end do
+      do i = 1, N
+          do j = 1, N
+                  m2(i,j) = k;
+                  k = k + 1.d0;
+          end do
+      end do
+call cpu_time(start)
+      call mult2(m1,m2,result,error)
+call cpu_time(finish)
+      WRITE(*,*) result .eq. matmul(m1,m2)
+      print '("Time = ", f6.3," seconds.")',finish-start
+      if (allocated(m1)) deallocate(m1)
+      if (allocated(m2)) deallocate(m2)
+      if (allocated(result)) deallocate(result)
+
+  end subroutine
+
+
+  subroutine mult10(m1, m2, result, error)
+
+      real, intent(in) :: m1(:, :), m2(:, :)
+      real, intent(out) :: result(:, :)
+      integer, intent(out) :: error
+      integer :: shape1(2), shape2(2), shape3(2)
+      error = 0
+      shape1 = shape(m1)
+      shape2 = shape(m2)
+      shape3 = shape(result)
+
+      if (shape1(2) .NE. shape2(1)) then
+          error = 1
+          return
+      endif
+
+      if ((shape3(1) .NE. shape1(1)) .OR. (shape3(2) .NE. shape2(2))) then
+          error = 2
+          return
+      endif
+
+      result = matmul(m1,m2)
+  end subroutine
+
     subroutine mult1(m1, m2, result, error)
 
         real, intent(in) :: m1(:, :), m2(:, :)
--- testa.f90
+++ testa.f90
@@ -1,46 +1,40 @@
 program main
   use mult
   implicit none
-!  real :: m1(2,3), m2(3,1)
-!  real :: result(2,1)
-real :: m1(2,3), m2(3,1),result(2,1)
+
+
   integer :: error
   WRITE(*,*) "m1"
-  m1(1,1) = 1;
-  m1(1,2) = 2;
-  m1(2,1) = 3;
-  m1(2,2) = 4;
-  m1(1,3) = 9;
-  m1(2,3) = 10;
-  m2(1,1) = 5;
-  m2(2,1) = 6;
-  m2(3,1) = 7;
 
-  WRITE(*,*) m1(1,:)
-  WRITE(*,*) m1(2,:)
-  WRITE(*,*) "m2"
-
-  WRITE(*,*) m2(1,:)
-  WRITE(*,*) m2(2,:)
-    WRITE(*,*) "aaa1"
-  call mult1(m1,m2,result,error)
-    WRITE(*,*) result(1,:)
-    WRITE(*,*) result(2,:)
-      WRITE(*,*) "aaa2"
-    call mult2(m1,m2,result,error)
-    WRITE(*,*) result(1,:)
-    WRITE(*,*) result(2,:)
-        WRITE(*,*) "aaa3"
-      call mult3(m1,m2,result,error)
-      WRITE(*,*) result(1,:)
-      WRITE(*,*) result(2,:)
-          WRITE(*,*) "aaa4"
-        call mult4(m1,m2,result,error)
-        WRITE(*,*) result(1,:)
-        WRITE(*,*) result(2,:)
-            WRITE(*,*) "aaa"
-                result = matmul(m1,m2)
-                WRITE(*,*) result(1,:)
-                WRITE(*,*) result(2,:)
-
+  makeSimulation(5);
 end
+function makeSimulation(N)
+  implicit none
+  use mult
+    real,allocatable :: m1(:,:), m2(:,:);
+    real result(:,:);
+    integer, intent(in) :: N;
+    integer :: i,j;
+    real (kind = 8) :: k;
+
+    allocate(m1(N,N));
+    allocate(m2(N,N));
+    k=1.d0;
+    do i = 1, N
+        do j = 1, N
+                m1(i,j) = k;
+                k = k + 1.d0;
+        end do
+    end do
+    do i = 1, N
+        do j = 1, N
+                m2(i,j) = k;
+                k = k + 1.d0;
+        end do
+    end do
+
+    call mult2(m1,m2,result,error)
+    WRITE(*,*) result
+    if(allocated(m1)) (deallocate(m1))
+
+end subroutine
